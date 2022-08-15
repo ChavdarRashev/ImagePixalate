923bbe1873fcb36a99d54d65a449c0d404c4a351
Chavdar Rashev <rashev@abv.bg>
2022-08-16 00:20:04 +0300

    Temp commit

--- ImagePixalate/ImagePixalate/Controllers/HomeController.cs
+++ ImagePixalate/ImagePixalate/Controllers/HomeController.cs
@@ -4,8 +4,13 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Drawing.Image;
 
 namespace ImagePixalate.Controllers
 {
@@ -20,13 +25,41 @@ namespace ImagePixalate.Controllers
 
         public IActionResult Index()
         {
+        
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult NewImg(string coordinates)
+        {
+            var list = JsonSerializer.Deserialize<List<Coordinates>>(coordinates);
+
+            using (Image img = Image.FromFile("wwwroot/500.jpg"))
+            using (Graphics g = Graphics.FromImage(img))
+
+
+            using (SolidBrush br = new SolidBrush(Color.Black))
+            {
+               // g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                foreach (Coordinates coor in list)
+                {
+                    g.FillRectangle(br, coor.topX, coor.topY, coor.width, coor.heigh);
+                }
+             
+
+                img.Save("wwwroot/YourNewFile.png");
+            }
+
+            return RedirectToAction(nameof(New));
+            
+        }
+
+        public IActionResult New()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ ImagePixalate/ImagePixalate/Models/coordianates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImagePixalate.Models
+{
+    public class Coordinates
+    {
+        public int topX { get; set; }
+        public int topY { get; set; }
+        public int width { get; set; }
+        public int heigh { get; set; }
+    }
+}
